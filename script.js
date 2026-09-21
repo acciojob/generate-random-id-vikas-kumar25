74b2e027e319e22e3ffd15daebc748f5702a26f5
Vikash Kumar <vikaskumarkxz@gmail.com>
2026-09-22 02:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	let res="";
+	for(let i=0; i<l; i++){
+		let random =  Math.floor(Math.random()*res.length);
+		res = res + char_list.charAt(random);
+	}
+	return res;
 }
 
 // Do not change the code below.
